002d8da15cf58fd9027d27cab1c7a3333ece371a
= <=>
2024-06-10 19:08:49 +0530

    Graph Construction

--- DeepRL/Data/Circuits/c17.v
+++ DeepRL/Data/Circuits/c17.v
@@ -1,18 +1,23 @@
-\m4_TLV_version 1d: tl-x.org
-\SV
-   // A Fibonacci Sequence example.
-   // Each cycle we generate a new number in the sequence,
-   // where each new value is the sum of the previous two.
-   // (1, 1, 2, 3, 5, 8, ...)
+// Verilog
+// c17
+// Ninputs 5
+// Noutputs 2
+// NtotalGates 6
+// NAND2 6
 
-   m4_makerchip_module
-\TLV
-!  $reset = *reset;
+module c17 (N1,N2,N3,N6,N7,N22,N23);
 
-   // =========================================
-   // Fibonacci.
-   $val[15:0] = $reset ? 1 : >>1$val + >>2$val;
-   // =========================================
+input N1,N2,N3,N6,N7;
 
-\SV
-   endmodule
+output N22,N23;
+
+wire N10,N11,N16,N19;
+
+nand NAND2_1 (N10, N1, N3);
+nand NAND2_2 (N11, N3, N6);
+nand NAND2_3 (N16, N2, N11);
+nand NAND2_4 (N19, N11, N7);
+nand NAND2_5 (N22, N10, N16);
+nand NAND2_6 (N23, N16, N19);
+
+endmodule
